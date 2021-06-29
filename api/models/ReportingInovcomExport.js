a99dee9f8054b6f4da5d957edd77f2fd6fcfd9c8
odilon01421 <orirandia@gmail.com>
2021-06-29 17:19:30 +0300

    test 6

--- api/models/ReportingInovcomExport.js
+++ api/models/ReportingInovcomExport.js
@@ -5,8 +5,8 @@
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
 //const path_reporting = 'D:/Reporting/Reporting/REPORTING INOVCOM Type.xlsx';
-// const path_reporting = '/dev/prod/00-TOUS/TestReporting/REPORTING INOVCOM Type.xlsx';
-const path_reporting = 'D:/LDR8_1421_nouv/PROJET_FELANA/REPORTING INOVCOM Type.xlsx';
+const path_reporting = '/dev/prod/00-TOUS/TestReporting/REPORTING INOVCOM Type.xlsx';
+// const path_reporting = 'D:/LDR8_1421_nouv/PROJET_FELANA/REPORTING INOVCOM Type.xlsx';
 module.exports = {
   attributes: {
   },
