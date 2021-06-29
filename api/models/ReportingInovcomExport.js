357fdca028968b05d81eee8310c05188f1716f8e
odilon01421 <orirandia@gmail.com>
2021-06-29 14:02:58 +0300

    test 3 export inov2

--- api/models/ReportingInovcomExport.js
+++ api/models/ReportingInovcomExport.js
@@ -997,6 +997,9 @@ module.exports = {
       }
     });
     console.log(" Colnumber2"+collonne);*/
+    console.log(numeroLigne);
+    console.log(colonne);
+    console.log(nombre_ok_ko.ok);
     numeroLigne.getCell(colonnne).value = nombre_ok_ko.ok;
     //numeroLigne.getCell(collonne).value = nombre_ok_ko.ko;
     await newWorkbook.xlsx.writeFile(path_reporting);
