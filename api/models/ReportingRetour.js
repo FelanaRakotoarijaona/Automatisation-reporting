429512caf5734a62bfdab9f5b9ffd48c466c7308
Felana RAKOTOARIJAONA <08059@easytech.mg>
2021-07-08 10:37:32 +0300

    coreectionretour85555555'

--- api/models/ReportingRetour.js
+++ api/models/ReportingRetour.js
@@ -103,10 +103,11 @@ module.exports = {
     }
    else if(table[nb]=="coldrcbtppublic")
     {
-      console.log('hehe coldrcbtppublic');
-      var tab = [];
-      tab = ReportingRetour.lectureEtInsertiontype22( trameflux,feuil,cellule,table,cellule2,nb,numligne,callback);
-      var sql = "insert into "+table[nbe]+" (nbok,nbko) values ('"+nbrok+"','"+nbrko+"') ";
+      console.log('ato v coldrcbtppublic');
+      /*var tab = [];
+      tab = ReportingRetour.lectureEtInsertiontype22( trameflux,feuil,cellule,table,cellule2,nb,numligne,callback);*/
+      //var sql = "insert into "+table[nbe]+" (nbok,nbko) values ('"+nbrok+"','"+nbrko+"') ";
+      var sql = "insert into "+table[nbe]+" (nbok,nbko) values ('0','1') ";
                       ReportingInovcom.getDatastore().sendNativeQuery(sql, function(err,res){
                         if(err)
                         {
