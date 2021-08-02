295687dafb12ddc05ac84c8c56a0bd2330f5fd92
Felana RAKOTOARIJAONA <08059@easytech.mg>
2021-08-02 15:15:53 +0300

    'frffr'

--- api/models/ReportingInovcom.js
+++ api/models/ReportingInovcom.js
@@ -213,10 +213,11 @@
     tab = ReportingInovcom.lectureEtInsertionSanteclairAudio(trameflux,feuil,cellule,table,cellule2,nb,numligne,callback);
     var nbe= parseInt(nb);
     console.log(tab);
-    var sql = "insert into "+table[nbe]+" (nbok,nbko) values ('"+tab[0]+"','"+tab[1]+"')";
+    var sql = "insert into "+table[nb]+" (typologiedelademande,okko) values ('"+tab[0]+"','"+tab[1]+"')";
     ReportingInovcom.getDatastore().sendNativeQuery(sql, function(err,res){
       if (err) { 
         console.log("Une erreur ve ok?");
+        console.log(err);
         //return callback(err);
        }
       else
@@ -232,7 +233,7 @@
     tab = ReportingInovcom.lectureEtInsertiontype4(trameflux,feuil,cellule,table,cellule2,nb,numligne,callback);
     var nbe= parseInt(nb);
     console.log(tab);
-    var sql = "insert into "+table[nbe]+" (nbok,nbko) values ('"+tab[0]+"','"+tab[1]+"')";
+    var sql = "insert into "+table[nbe]+" (nbok,nbko) values (typologiedelademande,okko) values ('"+tab[0]+"','"+tab[1]+"')";
     ReportingInovcom.getDatastore().sendNativeQuery(sql, function(err,res){
       if (err) { 
         console.log("Une erreur ve ok?");
@@ -252,7 +253,7 @@
     tab = ReportingInovcom.lectureEtInsertionEtatdesRestes(trameflux,feuil,cellule,table,cellule2,nb,numligne,callback);
     var nbe= parseInt(nb);
     console.log(tab);
-    var sql = "insert into "+table[nbe]+" (nbok,nbko) values ('"+tab[0]+"','"+tab[1]+"')";
+    var sql = "insert into "+table[nbe]+" (nbok,nbko) values (typologiedelademande,okko) values ('"+tab[0]+"','"+tab[1]+"')";
     ReportingInovcom.getDatastore().sendNativeQuery(sql, function(err,res){
       if (err) { 
         console.log("Une erreur ve ok?");
