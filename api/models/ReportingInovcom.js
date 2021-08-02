72fe2abc9ef164846e5d69407348aae7648964c1
Felana RAKOTOARIJAONA <08059@easytech.mg>
2021-08-02 15:24:00 +0300

    'grgr54'

--- api/models/ReportingInovcom.js
+++ api/models/ReportingInovcom.js
@@ -99,6 +99,7 @@
 
             var ok = 'Facture réglée';
             const regex = new RegExp(ok,'i');
+            console.log(desired_value1 + '<' + desired_value2);
             if(regex.test(desired_value) && desired_value2>desired_value1)
             {
               nbr=nbr + 1;
@@ -107,8 +108,8 @@
             {
                 nbrko=nbrko + 1;
               
-            }
-          }
+            };
+          };
       }
       else
       {
