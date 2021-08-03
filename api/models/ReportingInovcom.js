242e410efda68fd98836b9bcf0521eb9a8b51079
Felana RAKOTOARIJAONA <08059@easytech.mg>
2021-08-03 10:27:56 +0300

    'frffr66'

--- api/models/ReportingInovcom.js
+++ api/models/ReportingInovcom.js
@@ -2149,8 +2149,8 @@ lectureEtInsertiontype4v2:function(trameflux,feuil,cellule,table,cellule2,nb,num
                     }
                    
                     }
-                    console.log('fin'+ re);
-                    var sql = "insert into recherchefactureinteriale (typologiedelademande,okko) values ("+re+","+re+") ";
+                    
+                    /*var sql = "insert into recherchefactureinteriale (typologiedelademande,okko) values ("+re+","+re+") ";
                     ReportingInovcom.getDatastore().sendNativeQuery(sql, function(err,res){
                      if (err) { 
                        //console.log(err);
@@ -2162,9 +2162,10 @@ lectureEtInsertiontype4v2:function(trameflux,feuil,cellule,table,cellule2,nb,num
                        console.log(sql);
                        return callback(null, true);
                      };        
-                                         });     
+                                         });   */  
                
             });
+            console.log('fin'+ re);
           }
         
         }
