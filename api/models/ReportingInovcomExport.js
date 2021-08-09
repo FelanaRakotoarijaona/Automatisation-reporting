155dc97ebdd8999a94e690cd1ce0944354464b31
Felana RAKOTOARIJAONA <08059@easytech.mg>
2021-08-09 13:01:40 +0300

    'frfr546353563652636+5566656+6+56656556'

--- api/models/ReportingInovcomExport.js
+++ api/models/ReportingInovcomExport.js
@@ -11,6 +11,93 @@ const path_reporting = '/dev/prod/00-TOUS/TestReporting/REPORTING INOVCOM Type.x
 module.exports = {
   attributes: {
   },
+  ecritureOkKoNouveau : async function (nombre_ok_ko, table,date_export,mois1,callback) {
+    if(nombre_ok_ko.ok==null && nombre_ok_ko.ko==null)
+    {
+     console.log('ok' + nombre_ok_ko.ok);
+     console.log('ko' + nombre_ok_ko.ko);
+     return callback(null, "KO");
+    }
+    else
+    {
+     const Excel = require('exceljs');
+     const newWorkbook = new Excel.Workbook();
+     try{
+       await newWorkbook.xlsx.readFile(path_reporting);
+     const newworksheet = newWorkbook.getWorksheet(mois1);
+     var colonneDate = newworksheet.getColumn('A');
+     var ligneDate1;
+     var ligneDate;
+     colonneDate.eachCell(function(cell, rowNumber) {
+       var dateExcel = ReportingExcel.convertDate(cell.text);
+       if(dateExcel==date_export)
+       {
+         ligneDate1 = parseInt(rowNumber);
+         var line = newworksheet.getRow(ligneDate1);
+         var f = line.getCell(3).value;
+         var mc1 = "SANTECLAIR";
+         const regex = new RegExp(mc1,'i');
+         if(regex.test(f))
+         {
+           ligneDate = parseInt(rowNumber);
+         }
+       }
+     });
+     var rowDate = newworksheet.getRow(ligneDate);
+     var numeroLigne = rowDate;
+     var iniValue = ReportingExcel.getIniValue(table);
+     
+     var a5;
+ 
+     var rowm = newworksheet.getRow(1);
+     var colonnne;
+     var colDate1;
+     /*rowm.eachCell(function(cell, colNumber) {
+       if(cell.value == 'DOCUMENTS SAISIS')
+       {
+         colDate1 = parseInt(colNumber);
+         var man = newworksheet.getRow(3);
+         var f = man.getCell(colDate1).value;
+         if(f == iniValue.ok)
+         {
+           colonnne = parseInt(colNumber);
+         }
+         }
+     });
+     console.log(" Colnumber"+colonnne);
+     var collonne;
+     var colDate2;
+     rowm.eachCell(function(cell, colNumber) {
+       if(cell.value == 'DOCUMENTS TRAITES NON SAISIS (RETOURS)')
+       {
+         colDate2 = parseInt(colNumber);
+         var man = newworksheet.getRow(3);
+         var f = man.getCell(colDate2).value;
+         if(f == iniValue.ok)
+         {
+           collonne = parseInt(colNumber);
+         }
+       }
+     });
+     console.log(" Colnumber2 "+collonne);*/
+     numeroLigne.getCell('BP').value = nombre_ok_ko.ok;
+     numeroLigne.getCell('DR').value = nombre_ok_ko.ko;
+ 
+     
+     await newWorkbook.xlsx.writeFile(path_reporting);
+     sails.log("Ecriture OK KO terminé"); 
+     return callback(null, "OK");
+   
+     }
+     catch
+     {
+       console.log("Une erreur s'est produite");
+       Reportinghtp.deleteToutHtp(table,3,callback);
+     }
+    }
+   
+    
+     },
   // Récuperer nombre okko
   countok : function (table, callback) {
     const Excel = require('exceljs');
